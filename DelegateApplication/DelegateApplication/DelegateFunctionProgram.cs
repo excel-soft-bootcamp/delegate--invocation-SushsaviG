9e9a0a7ed66dfe61e91eb905de897aafb7e65671
SushsaviG <86051136+SushsaviG@users.noreply.github.com>
2021-07-10 13:57:48 +0530

    Add files via upload

---
+++ DelegateApplication/DelegateApplication/DelegateFunctionProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateApplication
+{
+    public class DelegateFunctionProgram
+    {
+        static double Calculate(int x, double y) { return x + y; }
+
+        void Update(int newNumber) { System.Console.WriteLine(newNumber); }
+        void Delete(string key) { }
+        public void function() { }
+
+        public void function2(string data)
+        {
+
+        } 
+        
+        public int function3(int data) { return data; }
+        public bool function4(string data ,string _data)
+        {
+            return true;
+        }
+        public double Funnction5(int data, double _data)
+        {
+            return(data) ;
+        }
+
+        static void FunctionsAsAnArguments(Action obj, Action<string> obj2, Func<int, int> obj3, Func<string, string, bool> obj4, Func<int,double,double> obj5,Action<int>obj6)
+        {
+            obj.Invoke();
+            obj2.Invoke("swathi");
+            obj3.Invoke(45);
+            obj4.Invoke("swa", "AAA");
+            obj5.Invoke(34, 33);
+            obj6.Invoke(77);
+
+
+
+
+
+        }
+    }
+}
---
+++ DelegateApplication/DelegateApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateFunctionProgram _delegate = new DelegateFunctionProgram();
+            
+            Action<int> Object1 = new Action<int>(Calculate);
+            Action<string> Object2 = new Action<string>(_delegate.function2);
+            Action object3 = new Action(_delegate.function);
+            Func<int, double, double> Object4 = new Func<int, double, double>(_delegate.Funnction5);
+            Func<int, int> Object5 = new Func<int, int>(_delegate.function3);
+            Func<string, string, bool> Object6 = new Func<string, string, bool>(_delegate.function4);
+            Func<int, double, double> Object7 = new Func<int, double, double>(_delegate.Funnction5);
+
+            FunctionsAsAnArguments(Object1, Object2, object3, Object4, Object5, Object6, Object7);
+        }
+    }
+}
